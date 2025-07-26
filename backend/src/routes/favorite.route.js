1f455291d676df2add46b9baa0f6f799db59a041
Addis <addis.ugr-4143-15@aau.edu.et>
2025-07-26 14:28:47 +0300

    fix: minor route bug in favorite toggle

--- backend/src/routes/favorite.route.js
+++ backend/src/routes/favorite.route.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 import { favAddRemove } from "../controllers/fav.controller.js";
 
 const favRoute = Router();
-productRoute.post("/:id", favAddRemove);
-export default productRoute;
+favRoute.post("/:id", favAddRemove);
+export default favRoute;
--- backend/src/routes/rootRoute.route.js
+++ backend/src/routes/rootRoute.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import productRoute from "./product.route.js";
 import uploadRoute from "./upload.route.js";
+import favRoute from "./favorite.route.js";
 const rootRoute = Router();
 rootRoute.use("/products", productRoute);
 rootRoute.use("/upload", uploadRoute);
-rootRoute.use("/favorite");
+rootRoute.use("/favorite", favRoute);
 
 export default rootRoute;
